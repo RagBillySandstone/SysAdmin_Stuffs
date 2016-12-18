d800a279587e347c9cb21b3cb1dba77a6cdf78b9
Stephen Castaneda <stephen.c976@gmail.com>
2016-12-18 09:02:19 -0700

    add logic to change ownership of the file to the user

--- cphomes.sh
+++ cphomes.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+# copy a file to each user's home directory and grant ownership to
+# that user.
+
 for homedir in /home/* ; do
 	if [ -d "$homedir" ] ; then 
 		cp $1 "$homedir"
+		user=`ls -ld $homedir | awk '{print $3}'`
+		chown --verbose $user:$user $homedir/$1
 	fi
 done
 
